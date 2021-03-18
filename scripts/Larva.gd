2cb6f2eb8b456d83ce96e2613336c4fd0209b502
AtanaskaNikolova <nikolovaas11@gmail.com>
2021-03-18 21:34:15 +0000

    Smaller larva and large nb

--- scripts/Larva.gd
+++ scripts/Larva.gd
@@ -3,4 +3,4 @@ extends KinematicBody2D
 
 
 func _process(delta):
-	position.x += 120*delta 
+	position.x -= 120*delta 
--- scripts/Trees.gd
+++ scripts/Trees.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 
 const LarvaScn = preload("res://scenes/Larva.tscn")
 onready var nest = $Nest
-export var max_larva = 3
+export var max_larva = 100
 var nb_larva = 0
 
 func _ready():
@@ -15,7 +15,8 @@ func _on_LarvaTimer_timeout():
 	var larva = LarvaScn.instance()
 	add_child(larva)
 	larva.global_transform = nest.global_transform
-	$LarvaTimer.wait_time = rand_range(2,6)
+	$LarvaTimer.wait_time = rand_range(2,7)
+	$LarvaTimer.start()
 	if nb_larva < max_larva:
 		$LarvaTimer.start()
 	else: 
