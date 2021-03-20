a4ac4a6cbb27e547b8b80b9d6a9474dd84fc6f95
AtanaskaNikolova <nikolovaas11@gmail.com>
2021-03-20 15:54:37 +0000

    dead larva animation

--- scripts/Larva.gd
+++ scripts/Larva.gd
@@ -1,15 +1,22 @@
 extends KinematicBody2D
 
 export(int) var damage = 1
+var speed = 120
+var dead = false
+
 
 func _process(delta):
-	position.x -= 120*delta 
+	position.x -= speed*delta 
 
 
 func _on_Area2D_area_entered(area):
-	queue_free()
+	$AnimationPlayer.play("LarvaDead")
+	speed = 0
+	dead = true
 
 
 func _on_Area2D_body_entered(body: Node) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not dead:
 		body.damage(self)
+
+
